d105cd2d64f59ebafd603bd0d97ccfae597932af
Gary O' Donoghue <gary.odonoghue@gmail.com>
2014-05-10 14:04:49 +0100

    add ai logic

--- test/desktop/src/com/ladinc/glitchperfect/desktop/DesktopLauncher.java
+++ test/desktop/src/com/ladinc/glitchperfect/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ladinc.glitchperfect.GlitchPerfectGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1920;
+		config.height = 1080;
 		new LwjglApplication(new GlitchPerfectGame(), config);
+
 	}
 }
